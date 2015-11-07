d39a4fe63261e401d9cfc2eb61ae42dcc816d0e2
edgar_klepek <edgarklepek@gmail.com>
2015-11-07 13:52:27 +0100

    UI of main activity / game screen

--- app/src/main/java/edgar/de/chaptertwo/MainActivity.java
+++ app/src/main/java/edgar/de/chaptertwo/MainActivity.java
@@ -6,10 +6,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+    private int points;
+    private int round;
+    private int countdown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void newGame() {
+        points = 0;
+        round = 1;
+        initRound();
+    }
+
+    private void initRound() {
+       countdown = 10;
     }
 }
